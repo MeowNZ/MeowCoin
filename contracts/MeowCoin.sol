b0bdd50871ccf4a5a75114d6c62b73cb5bb5cc03
Matt <sifrawr@gmail.com>
2018-11-01 09:53:47 +1300

    renamed CatCash to MeowCoin

--- contracts/MeowCoin.sol
+++ contracts/MeowCoin.sol
@@ -2,10 +2,10 @@ pragma solidity ^0.4.23;
 
 import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
 
-contract CatCash is ERC20 {
+contract MeowCoin is ERC20 {
 
   string public symbol = "MEOW";
-  string public name = "CatCash";
+  string public name = "MeowCoin";
   uint8 public decimals = 18;
   uint8 public version = 1;
 
